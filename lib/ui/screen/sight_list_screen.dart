08a62d14a077434926e9b0c7462e621a113abed1
Poluektov Alexandr <pansoft@bk.ru>
2021-08-02 09:18:32 +0300

    Задание 4.4-1

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class SightListScreen extends StatefulWidget {
   const SightListScreen({Key key}) : super(key: key);
@@ -12,6 +11,25 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: false,
+        elevation: 0,
+        toolbarHeight: 154,
+        backgroundColor: Colors.white,
+        title: Container(
+          width: double.infinity,
+          child: Text(
+            'Список\nинтересных мест',
+            style: const TextStyle(
+              color: Color(0xFF3B3E5B),
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+              height: 1.11,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
