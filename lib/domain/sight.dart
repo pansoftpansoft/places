47c88f37902f0368d852a117f7f168a4a8a9342c
Poluektov Alexandr <pansoft@bk.ru>
2021-08-03 07:22:43 +0300

    Задание 4.6-1

---
+++ lib/domain/sight.dart
@@ -0,0 +1,10 @@
+class Sight {
+  final String name; //название достопримечательности
+  final String lat; //координаты места
+  final String lon; //координаты места
+  final String url; //путь до фотографии в интернете
+  final String details; //описание достопримечательности
+  final String type; //тип достопримечательности.
+
+  Sight(this.name, this.lat, this.lon, this.url, this.details, this.type);
+}
