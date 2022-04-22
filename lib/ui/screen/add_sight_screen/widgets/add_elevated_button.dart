f06bbbef4c3b58d3544900669cbc0093ab88e8bd
poluektov <pansoft@bk.ru>
2022-04-22 16:46:53 +0300

    Задание 8.6-1 рефакторинг. На форме "добавить мест" сделал алерт о добавлении места.

--- lib/ui/screen/add_sight_screen/widgets/add_elevated_button.dart
+++ lib/ui/screen/add_sight_screen/widgets/add_elevated_button.dart
@@ -6,6 +6,7 @@ import 'package:places/ui/res/labels.dart';
 import 'package:places/ui/res/sizes.dart';
 import 'package:places/ui/screen/add_sight_screen/models/add_sight_model.dart';
 import 'package:places/ui/screen/add_sight_screen/widgets/bottom_sheet_create_button_row.dart';
+import 'package:places/ui/screen/add_sight_screen/widgets/show_alert_add.dart';
 import 'package:places/ui/screen/sight_list_screen/models/sight_list_screen_model.dart';
 import 'package:provider/provider.dart';
 
@@ -41,8 +42,15 @@ class AddElevatedButton extends StatelessWidget {
                 mocks.add(
                   sight,
                 );
-                context.read<SightListScreenModel>().updateSightList();
-                Navigator.pop(context);
+                Future(() async {
+                  await showDialog<void>(
+                    context: context,
+                    builder: (final context) => const ShowAlertAdd(),
+                  );
+                  context.read<SightListScreenModel>().updateSightList();
+                  Navigator.pop(context);
+                  context.read<AddSightModel>().disableButton = null;
+                });
               },
       ),
     );
---
+++ lib/ui/screen/add_sight_screen/widgets/alert_dialog_ok_button.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:places/ui/res/color_palette.dart';
+import 'package:places/ui/res/labels.dart';
+import 'package:places/ui/res/sizes.dart';
+
+///Диалог придупреждения при нажатии кнопки отмена
+class AlertDialogOkButton extends StatelessWidget {
+  static double heightAlertDialog = 48;
+
+  ///Диалог придупреждения при нажатии кнопки отмена
+  const AlertDialogOkButton({
+    final Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(final BuildContext context) => Padding(
+        padding: const EdgeInsets.only(
+          top: paddingPage / 2,
+        ),
+        child: ConstrainedBox(
+          constraints: BoxConstraints.tightFor(
+            height: heightAlertDialog,
+          ),
+          child: ElevatedButton(
+            style: _buildButtonStyle(),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  ok.toUpperCase(),
+                  style: Theme.of(context).textTheme.headline5!.copyWith(
+                        color: ColorPalette.greenColor,
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  ButtonStyle _buildButtonStyle() {
+    return ButtonStyle(
+      padding: MaterialStateProperty.all(EdgeInsets.zero),
+      elevation: MaterialStateProperty.all(0),
+      backgroundColor:
+          MaterialStateProperty.all<Color>(ColorPalette.dmBasicColor),
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+        const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(
+              borderRadiusCard16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
