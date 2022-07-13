19059740d5ad596ed572c959cad41369312370bf
poluektov <pansoft@bk.ru>
2022-07-13 08:52:04 +0300

    Задание 13.5-1 MobX рефакторинг

--- lib/ui/screen/list_places_screen/widgets/list_places_screen_portrait.dart
+++ lib/ui/screen/list_places_screen/widgets/list_places_screen_portrait.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobx/mobx.dart';
+import 'package:places/data/interactor/filters_screen_interactor.dart';
 import 'package:places/data/repository/place_repository.dart';
 import 'package:places/store/list_places/list_places_store.dart';
 import 'package:places/type_place.dart';
@@ -33,57 +34,67 @@ class _ListPlacesScreenPortraitState extends State<ListPlacesScreenPortrait> {
 
   @override
   Widget build(BuildContext context) {
-    return Provider<ListPlacesStore>(
-      create: (_) => _store,
-      child: Observer(
-        builder: (_) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: paddingPage,
-            ),
-            child: CustomScrollView(
-              slivers: <Widget>[
-                SliverPersistentHeader(
-                  delegate: StickyHeader(),
-                  pinned: true,
-                ),
-                if (_store.getListPlaceFuture?.status == FutureStatus.pending)
-                  SliverToBoxAdapter(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        top: heightSizeBox12,
-                        bottom: iconSize29,
-                      ),
-                      child: Image.asset(
-                        ellipse107,
-                        height: iconSize29,
-                        width: iconSize29,
-                      ),
+    return Consumer<FiltersScreenInteractor>(
+      builder: (
+        final context,
+        final cart,
+        final child,
+      ) { return
+            Provider<ListPlacesStore>(
+              create: (_) => _store,
+              child: Observer(
+                builder: (_) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: paddingPage,
                     ),
-                  )
-                else
-                  SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                          (final context,
-                          final index,) {
-                        debugPrint(
-                          ' mocksFiltered[$index].isFavorites = '
-                          '${mocksFiltered[index].isFavorites}',
-                        );
+                    child: CustomScrollView(
+                      slivers: <Widget>[
+                        SliverPersistentHeader(
+                          delegate: StickyHeader(),
+                          pinned: true,
+                        ),
+                        if (_store.getListPlaceFuture?.status ==
+                            FutureStatus.pending)
+                          SliverToBoxAdapter(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                top: heightSizeBox12,
+                                bottom: iconSize29,
+                              ),
+                              child: Image.asset(
+                                ellipse107,
+                                height: iconSize29,
+                                width: iconSize29,
+                              ),
+                            ),
+                          )
+                        else
+                          SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (
+                                final context,
+                                final index,
+                              ) {
+                                debugPrint(
+                                  ' mocksFiltered[$index].isFavorites = '
+                                  '${mocksFiltered[index].isFavorites}',
+                                );
 
-                        return CardPlace(mocksFiltered[index]);
-                      },
-                      childCount: mocksFiltered.length,
+                                return CardPlace(mocksFiltered[index]);
+                              },
+                              childCount: mocksFiltered.length,
+                            ),
+                          ),
+                      ],
                     ),
-                  ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
+                  );
+                },
+              ),
+            );
+          },
+        );
+ //     },
+ //   );
   }
-
-  //);
-//}
 }
